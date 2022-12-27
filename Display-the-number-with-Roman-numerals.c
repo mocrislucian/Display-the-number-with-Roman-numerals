d31535a765b57eac8fcaabe2f2edc6dacfad9469
Mocriș Lucian <mocrislucian777@gmail.com>
2022-12-27 12:42:08 +0200

    First implementation

---
+++ Display-the-number-with-Roman-numerals.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+#define READnumber(x) scanf("%d", &x);
+
+int main(void)
+{
+    int number;
+
+    printf("Enter your number: ");
+    READnumber(number);
+
+    return 0;
+}
