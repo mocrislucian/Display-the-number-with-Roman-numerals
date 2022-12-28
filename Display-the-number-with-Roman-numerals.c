ae064e2dc692f865df5fa1ff95dda413ec4bd112
Mocriș Lucian <mocrislucian777@gmail.com>
2022-12-28 12:42:54 +0200

    Implementation of the remaining cases: 10 to 1000

--- Display-the-number-with-Roman-numerals.c
+++ Display-the-number-with-Roman-numerals.c
@@ -12,7 +12,55 @@ int main(void)
 
     while(number != 0)
     {
-        if(number >= 10)
+        if(number >= 1000)
+        {
+            printf("M");
+            number -= 1000;
+        }
+
+        else if(number >= 900)
+        {
+            printf("CM");
+            number -= 900;
+        }
+
+        else if(number >= 500)
+        {
+            printf("D");
+            number -= 500;
+        }
+
+        else if(number >= 400)
+        {
+            printf("CD");
+            number -= 400;
+        }
+
+        else if(number >= 100)
+        {
+            printf("C");
+            number -= 100;
+        }
+
+        else if(number >= 90)
+        {
+            printf("XC");
+            number -= 90;
+        }
+
+        else if(number >= 50)
+        {
+            printf("L");
+            number -= 50;
+        }
+
+        else if(number >= 40)
+        {
+            printf("XL");
+            number -= 40;
+        }
+
+        else if(number >= 10)
         {
             printf("X");
             number -= 10;
